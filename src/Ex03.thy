d31fc32857270e40d187a1fc392bb7196a727c2e
Sven Wille <wille.sven.public@gmail.com>
2017-04-28 07:48:47 +0200

    added solution Ex03

---
+++ src/Ex03.thy
@@ -0,0 +1,18 @@
+theory Ex03 
+imports Main 
+begin 
+
+
+
+lemma "\<not>A \<longrightarrow> A \<longrightarrow> B"
+proof - 
+{
+  assume "\<not>A"
+  {
+    assume A
+    with \<open>\<not>A\<close> have B by (rule notE)
+  }
+  hence "A \<longrightarrow> B" by (rule impI)
+}
+thus ?thesis by (rule impI)
+qed
